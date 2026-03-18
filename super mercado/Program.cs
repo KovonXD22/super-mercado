7430206c1d9ba0a53020666f439e1f9fc55f1531
KovonXD26 <kevin.cea@catolica.edu.sv>
2026-03-18 11:24:54 -0600

    Agregar archivos de proyecto.

---
+++ super mercado/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+
+class supermercado
+{
+    public static void Main(string[] args)
+    {
+        int opcion;
+        string newproduct;
+        Console.WriteLine("Ingrese al rol que le corresponda");
+        Console.WriteLine("1. Cliente");
+        Console.WriteLine("2. Administrador");
+        opcion = int.Parse(Console.ReadLine());
+
+        switch (opcion)
+        {
+            case 1:
+                ArrayList productos = new ArrayList() { "frutas", "carnes", "productos de limpieza", "bebidas" };
+                foreach (string p in productos)
+                    Console.WriteLine(p);
+                Console.WriteLine("Ingrese el producto a agregar al carrito");
+                newproduct = Console.ReadLine();
+                if (productos.Contains(newproduct))
+                {
+                    Console.WriteLine($"{newproduct} agregado con éxito");
+                 
+                }
+                else
+                {
+                    Console.WriteLine("Opción no disponible");
+                }
+                Console.WriteLine("------Menú------");
+                Console.WriteLine(newproduct);
+
+                break;
+
+            case 2:
+                Console.WriteLine("Hola");
+                break;
+
+            default:
+                Console.WriteLine("Error");
+                break;
+        }
+       
+
+
+
+
+}
+}

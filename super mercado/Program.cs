d9b7a1b1d234e83b8bfb8a713bebb4387e1b7465
KovonXD26 <kevin.cea@catolica.edu.sv>
2026-03-18 14:32:42 -0600

    código actualizado

--- super mercado/Program.cs
+++ super mercado/Program.cs
@@ -4,8 +4,9 @@ class supermercado
 {
     public static void Main(string[] args)
     {
-        int opcion;
+        int opcion, passw, opcionadmin;
         string newproduct;
+
         Console.WriteLine("Ingrese al rol que le corresponda");
         Console.WriteLine("1. Cliente");
         Console.WriteLine("2. Administrador");
@@ -16,25 +17,80 @@ class supermercado
             case 1:
                 ArrayList productos = new ArrayList() { "frutas", "carnes", "productos de limpieza", "bebidas" };
                 foreach (string p in productos)
-                    Console.WriteLine(p);
+                    Console.WriteLine($"*{p}");
                 Console.WriteLine("Ingrese el producto a agregar al carrito");
                 newproduct = Console.ReadLine();
                 if (productos.Contains(newproduct))
                 {
                     Console.WriteLine($"{newproduct} agregado con éxito");
-                 
+                    Console.WriteLine("------Menú------");
+                    Console.WriteLine(newproduct);
                 }
                 else
                 {
                     Console.WriteLine("Opción no disponible");
                 }
-                Console.WriteLine("------Menú------");
-                Console.WriteLine(newproduct);
-
                 break;
 
             case 2:
-                Console.WriteLine("Hola");
+                Console.WriteLine("Ingrese datos de usuario");
+                Console.WriteLine("Usuario:");
+                string usuario = Console.ReadLine();
+                Console.WriteLine("Contraseña:");
+                passw= int.Parse(Console.ReadLine());
+                if (usuario == "Admin")
+                {
+                    if (passw == 123)
+                    {
+                        Console.WriteLine("------Acceso permitido------");
+                        ArrayList productosadmin = new ArrayList() { "frutas", "carnes", "productos de limpieza", "bebidas" };
+                        foreach (string p in productosadmin)
+                            Console.WriteLine($"*{p}");
+                        Console.WriteLine("------Menú de opciones------");
+
+                        Console.WriteLine("1. Agregar producto");
+                        Console.WriteLine("2. Eliminar producto");
+                        opcionadmin = int.Parse(Console.ReadLine());
+                        switch (opcionadmin)
+                        {
+                            case 1:
+                                Console.WriteLine("Ingrese el producto que desea agregar al almacén");
+                                string newproductadmin = Console.ReadLine();
+                                productosadmin.Insert(3, newproductadmin);
+                                Console.WriteLine("------NewMenu------");
+                                foreach (string p in productosadmin)
+                                    Console.WriteLine($"*{p}");
+                                break;
+                            case 2:
+                                Console.WriteLine("Ingrese el producto que desea eliminar");
+                                string removeproduct = Console.ReadLine();
+                                if (productosadmin.Contains(removeproduct))
+                                {
+                                    productosadmin.Remove(removeproduct);
+                                    Console.WriteLine("Producto eliminado");
+
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Producto no disponible");
+                                }
+                                
+                                break;
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Contraseña incorrecta");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Usuario incorrecto");
+                }
+                
+
                 break;
 
             default:
@@ -48,3 +104,4 @@ class supermercado
 
 }
 }
+//código modificado
